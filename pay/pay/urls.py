89eec2154f91d4e06674cec7b8d317f88da68179
ja-odur <odurjoseph8@gmail.com>
2022-01-10 14:08:00 +0300

    add pay django project

--- pay/pay/urls.py
+++ pay/pay/urls.py
@@ -1,4 +1,4 @@
-"""identity URL Configuration
+"""pay URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
@@ -22,9 +22,9 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Authentication API",
+      title="Pay API",
       default_version='v1',
-      description="Authentication API Docs",
+      description="Pay API Docs",
       # terms_of_service="https://www.google.com/policies/terms/",
       contact=openapi.Contact(email="odurjoseph8@gmail.com"),
       # license=openapi.License(name="BSD License"),
@@ -33,10 +33,8 @@ schema_view = get_schema_view(
    permission_classes=(permissions.AllowAny,),
 )
 
-
 urlpatterns = [
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
-    path('auth/', include('authentication.urls')),
 ]
