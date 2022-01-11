36b0ed28f4e815421f4f54bead642755313305fc
ja-odur <odurjoseph8@gmail.com>
2022-01-11 22:50:15 +0300

    add channels to payment models

--- pay/payment/models.py
+++ pay/payment/models.py
@@ -3,8 +3,7 @@ from django.db import models
 
 class Payment(models.Model):
     amount = models.DecimalField(max_digits=19, decimal_places=2)
-    sender = models.ForeignKey('authentication.MinimalUser', on_delete=models.CASCADE, related_name='sent_by')
-    receiver = models.ForeignKey('authentication.MinimalUser', on_delete=models.CASCADE, related_name='received_by')
-    channel = models.ForeignKey('channel.PayChannel', on_delete=models.CASCADE)
+    send_channel = models.ForeignKey('channel.PayChannel', on_delete=models.CASCADE, related_name='sent_by', null=True)
+    receive_channel = models.ForeignKey('channel.PayChannel', on_delete=models.CASCADE, related_name='received_by', null=True)
     reason = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
