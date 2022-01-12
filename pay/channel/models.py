21f69d4f017d609275b083a36225a4134127395a
ja-odur <odurjoseph8@gmail.com>
2022-01-12 09:58:03 +0300

    add unique together constraint

--- pay/channel/models.py
+++ pay/channel/models.py
@@ -11,9 +11,12 @@ class PayChannel(models.Model):
         ('USD', 'USD'),
     ]
     type = models.CharField(max_length=20, choices=TYPE_CHOICES)
-    name = models.CharField(max_length=15, unique=True)
+    name = models.CharField(max_length=15)
     amount = models.DecimalField(max_digits=19, decimal_places=2, default=5000)
     currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES)
     user = models.ForeignKey('authentication.MinimalUser', on_delete=models.CASCADE, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ['name', 'user']
