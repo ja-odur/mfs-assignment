8433be6790da205671e22392d96fee29d90024ea
ja-odur <odurjoseph8@gmail.com>
2022-01-12 08:37:03 +0300

    add transition redux state

---
+++ frontend/redux/MainAlert/reducers.js
@@ -0,0 +1,27 @@
+import {
+  UPDATE_ALERT_SUCCESS,
+  HIDE_ALERT_SUCCESS,
+  RESET_ALERT_SUCCESS,
+} from "./actions";
+
+const initialState = {
+  show: false,
+  message: "",
+  severity: "success",
+};
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case RESET_ALERT_SUCCESS:
+      return initialState;
+
+    case UPDATE_ALERT_SUCCESS:
+      return action.payload;
+
+    case HIDE_ALERT_SUCCESS:
+      return action.payload;
+
+    default:
+      return state;
+  }
+}
--- frontend/redux/reducers.js
+++ frontend/redux/reducers.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import userReducer from './user/reducer';
 import channelReducer from './channel/reducer'
 import paymentReducer from './payment/reducer'
+import mainAlertReducer from './MainAlert/reducers';
 
 const rootReducer = combineReducers({
     user: userReducer,
     channel: channelReducer,
     payment: paymentReducer,
+    mainAlert: mainAlertReducer ,
 });
 
 export { rootReducer as default };
